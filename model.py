007d8856501cb5318377c4cfcf3e5cced36134b2
Carter R <tac0c4t4life@gmail.com>
2025-10-28 13:07:13 -0700

    Update model.py

--- model.py
+++ model.py
@@ -6,23 +6,21 @@ from parse import parse
 class Network(nn.Module):
     def __init__(self, input_size: int, hidden_size: int = 32, output_size: int = 1):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.Threshold(-0.055, 0.04),
-            nn.Linear(hidden_size, output_size)
-        )
+        layers = []
+        layers.append(nn.Linear(input_size, hidden_size))
+        layers.append(nn.ReLU())
+
+        for _ in range(9):
+            layers.append(nn.Linear(hidden_size, hidden_size))
+            layers.append(nn.ReLU())
+
+        layers.append(nn.Linear(hidden_size, hidden_size))
+        layers.append(nn.Threshold(-0.055,0.04))
+
+        # Final output layer (no ReLU)
+        layers.append(nn.Linear(hidden_size, output_size))
+
+        self.net = nn.Sequential(*layers)
 
 
     def forward(self, x):
